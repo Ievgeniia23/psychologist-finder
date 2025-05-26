d9eeaa0b1b136b2b5ea1fc597eeff4b72269fb91
Ievgeniia23 <affari.nuovi.sempre@gmail.com>
2025-05-26 16:56:54 +0200

    Add Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 // import { useState } from 'react'
-import AppBar from './components/AppBar/AppBar';
+import { Routes, Route } from 'react-router-dom';
 
+import AppBar from './components/AppBar/AppBar';
+import Home from './pages/Home/Home.jsx';
+import Psychologists from "./pages/Psychologists/Psychologists.jsx"
 import './App.css';
 
 const App = () => {
   return (
     <div>
       <AppBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/psychologists" element={<Psychologists/> } />
+
+       
+      </Routes>
     </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import woman1x from "../../assets/images/woman_web@1x.webp"
+import woman2x from "../../assets/images/woman_web@2x.webp"
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <div>
+        <h1>
+          The road to the <span>depths</span> of the human soul
+        </h1>
+        <p>
+          We help you to reveal your potential, overcome challenges and find a
+          guide in your own life with the help of our experienced psychologists.
+        </p>
+        <button type="button">Get started</button>
+      </div>
 
-export default Home
+      <div>
+        <img
+          src={woman1x}
+          srcSet={`${woman1x} 1x, ${woman2x} 2x`}
+          alt="woman"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
