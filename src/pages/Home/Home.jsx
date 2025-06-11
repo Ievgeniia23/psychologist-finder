38f0c93154f76a4c47f7f0a43469b2d3cf25d3ba
Ievgeniia23 <affari.nuovi.sempre@gmail.com>
2025-06-11 12:33:59 +0200

    Add some styles for Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import woman1x from "../../assets/images/woman_web@1x.webp"
-import woman2x from "../../assets/images/woman_web@2x.webp"
-import icons from "../../assets/images/sprite.svg"
-import css from "./Home.module.css"
+import woman1x from '../../assets/images/woman_web@1x.webp';
+import woman2x from '../../assets/images/woman_web@2x.webp';
+import icons from '../../assets/images/sprite.svg';
+import css from './Home.module.css';
 
 const Home = () => {
   return (
-    <div>
-      <div>
+    <div className={css.homeWrapper}>
+      <div className={css.leftWrap}>
         <h1>
           The road to the <span className={css.italic}>depths</span> of the
           human soul
         </h1>
-        <p>
+        <p className={css.description}>
           We help you to reveal your potential, overcome challenges and find a
           guide in your own life with the help of our experienced psychologists.
         </p>
-        <button type="button">Get started</button>
+        <button className={css.buttonWrap} type="button">
+          Get started
+          <svg className={css.arrowStyle}>
+            <use href={icons + '#icon-arrow'}></use>
+          </svg>
+        </button>
       </div>
 
-      <div>
+      <div className={css.rightWrap}>
         <img
           className={css.woman}
           src={woman1x}
@@ -42,8 +47,10 @@ const Home = () => {
               <use href={icons + '#icon-fe_check'}></use>
             </svg>
           </div>
-          <p>Experienced psychologists</p>
-          <p>15,000</p>
+          <div className={css.informationWrap}>
+            <p className={css.paragraphStyle}>Experienced psychologists</p>
+            <p className={css.numberStyle}>15,000</p>
+          </div>
         </div>
       </div>
     </div>
